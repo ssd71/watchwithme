94f54f503ddf984129dda517ee1ab0609ae3589e
Shibani Shankar Dash <shibanidash71@gmail.com>
2019-06-30 01:09:32 +0530

    Set listening port to 8080

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = 8080;
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
@@ -12,4 +12,4 @@ app.get('/', (req, res) => {
 
 app.use(express.static('.'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`WatchWithMe app listening on port ${port}!`));
