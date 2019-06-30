ccb7267b9bdd61f981483ab9a15d0fa98aa03c36
Shibani Shankar Dash <shibanidash71@gmail.com>
2019-06-30 18:01:31 +0530

    Specify port number for express server

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
